af19d1df549c01c20896d329096cd2d6b3d281cb
vovtshik <vovtshik@gmail.com>
2021-08-06 17:12:53 +0300

    Added code to the body of the function main in file ex06_58.cpp

--- ex06_58.cpp
+++ ex06_58.cpp
@@ -5,8 +5,33 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+#include "ex06_58.h"
+
 int main()
 {
+   int int1, int2;
 
-    return 0;
+   cout << "Input two integer values: ";
+   cin >> int1 >> int2;
+
+   cout << "The minimum integer value is: "
+	<< min(int1, int2);
+
+   double double1, double2;
+
+   cout << "Input two double values: ";
+   cin >> double1 >> double2;
+
+   cout << "The minimum double value is: "
+	<< min(double1, double2);
+   
+   char char1, char2;
+
+   cout << "Input two char value: ";
+   cin >> char1 >> char2;
+
+   cout << "The minimum char value is: "
+	<< min(char1, char2);
+
+   return 0;
 }
